13b09fb188f9ab7eca8911905a077f075f6857b2
Ville Takanen <ville.takanen@foodie.fm>
2020-02-13 16:50:26 +0200

    0.1.1 added i18n and Vuetify

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
